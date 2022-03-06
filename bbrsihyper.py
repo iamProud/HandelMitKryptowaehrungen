be867475b71951b2dd03d05a6e33158eabc553dd
Sebastian Stolz <70321701+iamProud@users.noreply.github.com>
2022-03-06 12:56:20 +0100

    Create bbrsihyper.py

---
+++ bbrsihyper.py
@@ -0,0 +1,195 @@
+# pragma pylint: disable=missing-docstring, invalid-name, pointless-string-statement
+# flake8: noqa: F401
+
+# --- Do not remove these libs ---
+import numpy as np  # noqa
+import pandas as pd  # noqa
+from pandas import DataFrame
+
+from freqtrade.strategy import (BooleanParameter, CategoricalParameter, DecimalParameter,
+                                IStrategy, IntParameter)
+
+# --------------------------------
+# Add your lib to import here
+import talib.abstract as ta
+import freqtrade.vendor.qtpylib.indicators as qtpylib
+
+
+class BBRSIHYPER(IStrategy):
+    # Hyperopt Values
+    #buy
+    buy_rsi = IntParameter(5,50,default=30,space ="buy")
+    buy_rsi_enabled = CategoricalParameter([True,False], default = True, space = "buy")
+    buy_trigger = CategoricalParameter(['bb_lower1', 'bb_lower', 'bb_lower3', 'bb_lower4'], default = "bb_lower")
+
+    #sell
+    sell_rsi = IntParameter(30,100, default=50, space = "sell")
+    sell_rsi_enabled = CategoricalParameter([True,False], default = False, space = "sell")
+    sell_trigger = CategoricalParameter(['bb_lower1','bb_middle1','bb_upper1'], default = "sell")
+
+    # Strategy interface version - allow new iterations of the strategy interface.
+    # Check the documentation or the Sample strategy to get the latest version.
+    INTERFACE_VERSION = 2
+
+    # Minimal ROI designed for the strategy.
+    # This attribute will be overridden if the config file contains "minimal_roi".
+    minimal_roi = {
+         "0": 0.228,
+        "178": 0.173,
+        "529": 0.06,
+        "662": 0 
+    }
+
+    # Optimal stoploss designed for the strategy.
+    # This attribute will be overridden if the config file contains "stoploss".
+    stoploss = -0.294
+
+
+    # Trailing stoploss
+    trailing_stop = True
+    trailing_stop_positive = 0.146
+    trailing_stop_positive_offset = 0.21
+    trailing_only_offset_is_reached = False
+
+    # Optimal timeframe for the strategy.
+    timeframe = '1h'
+
+    # Run "populate_indicators()" only for new candle.
+    process_only_new_candles = False
+
+    # These values can be overridden in the "ask_strategy" section in the config.
+    #use_sell_signal = True
+    #sell_profit_only = False
+    #ignore_roi_if_buy_signal = False
+
+    # Number of candles the strategy requires before producing valid signals
+    #startup_candle_count: int = 30
+
+    # Optional order type mapping.
+    order_types = {
+        'buy': 'limit',
+        'sell': 'limit',
+        'stoploss': 'limit',
+        'stoploss_on_exchange': False
+    }
+
+    # Optional order time in force.
+    order_time_in_force = {
+        'buy': 'gtc',
+        'sell': 'gtc'
+    }
+
+    plot_config = {
+        # Main plot indicators (Moving averages, ...)
+        'main_plot': {
+            'tema': {},
+            'sar': {'color': 'white'},
+        },
+        'subplots': {
+            # Subplots - each dict defines one additional plot
+            "MACD": {
+                'macd': {'color': 'blue'},
+                'macdsignal': {'color': 'orange'},
+            },
+            "RSI": {
+                'rsi': {'color': 'red'},
+            }
+        }
+    }
+    def informative_pairs(self):
+        """
+        Define additional, informative pair/interval combinations to be cached from the exchange.
+        These pair/interval combinations are non-tradeable, unless they are part
+        of the whitelist as well.
+        For more information, please consult the documentation
+        :return: List of tuples in the format (pair, interval)
+            Sample: return [("ETH/USDT", "5m"),
+                            ("BTC/USDT", "15m"),
+                            ]
+        """
+        return []
+
+    def populate_indicators(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Adds several different TA indicators to the given DataFrame
+
+        Performance Note: For the best performance be frugal on the number of indicators
+        you are using. Let uncomment only the indicator you are using in your strategies
+        or your hyperopt configuration, otherwise you will waste your memory and CPU usage.
+        :param dataframe: Dataframe with data from the exchange
+        :param metadata: Additional information, like the currently traded pair
+        :return: a Dataframe with all mandatory indicators for the strategies
+        """
+
+        # Momentum Indicators
+        # ------------------------------------
+
+
+        # RSI
+        dataframe['rsi'] = ta.RSI(dataframe)
+
+
+        # Overlap Studies
+        # ------------------------------------
+
+        # Bollinger Bands
+        bollinger = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=2)
+        dataframe['bb_lowerband'] = bollinger['lower']
+        dataframe['bb_middleband'] = bollinger['mid']
+        dataframe['bb_upperband'] = bollinger['upper']
+
+        #For hyperopt
+        dataframe['sell-rsi'] = ta.RSI(dataframe)
+        #Boilinger Bands
+        bollinger1 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=1)
+        dataframe['bb_lowerband1'] = bollinger1['lower']
+        dataframe['bb_middleband1'] = bollinger1['mid']
+        dataframe['bb_upperband1'] = bollinger1['upper']
+
+
+        bollinger3 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=3)
+        dataframe['bb_lowerband3'] = bollinger3['lower']
+        dataframe['bb_middleband3'] = bollinger3['mid']
+        dataframe['bb_upperband3'] = bollinger3['upper']
+
+
+        bollinger4 = qtpylib.bollinger_bands(qtpylib.typical_price(dataframe), window=20, stds=4)
+        dataframe['bb_lowerband4'] = bollinger4['lower']
+        dataframe['bb_middleband4'] = bollinger4['mid']
+        dataframe['bb_upperband4'] = bollinger4['upper']
+
+
+
+
+        return dataframe
+
+    def populate_buy_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Based on TA indicators, populates the buy signal for the given dataframe
+        :param dataframe: DataFrame populated with indicators
+        :param metadata: Additional information, like the currently traded pair
+        :return: DataFrame with buy column
+        """
+        dataframe.loc[
+            (
+                (dataframe['rsi'] >= 10) &
+                (dataframe["close"] < dataframe['bb_lowerband1'] )
+            ),
+            'buy'] = 1
+
+        return dataframe
+
+    def populate_sell_trend(self, dataframe: DataFrame, metadata: dict) -> DataFrame:
+        """
+        Based on TA indicators, populates the sell signal for the given dataframe
+        :param dataframe: DataFrame populated with indicators
+        :param metadata: Additional information, like the currently traded pair
+        :return: DataFrame with buy column
+        """
+        dataframe.loc[
+            (
+                (dataframe['rsi'] >= 64) &
+                (dataframe["close"] > dataframe['bb_middleband1'] )
+            ),
+            'sell'] = 1
+        return dataframe
